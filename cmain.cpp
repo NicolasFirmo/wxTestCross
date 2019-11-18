fb13dfcf65a182473cd10b4be819ba841703f75e
Nicolas Firmo <nicolasfofo@hotmail.com>
2019-11-18 05:02:52 -0300

    Resize

--- cmain.cpp
+++ cmain.cpp
@@ -1,10 +1,10 @@
 #include "cmain.h"
 #include <iostream>
 
-cmain::cmain(/* args */) : wxFrame(nullptr, wxID_ANY, "Bora bora pessoal!", wxPoint(30, 30), wxSize(300, 600))
+cmain::cmain(/* args */) : wxFrame(nullptr, wxID_ANY, "Bora bora pessoal!", wxPoint(30, 30), wxSize(316, 600))
 {
-  m_btn1 = new wxButton(this, wxID_ANY, "bora bora", wxPoint(20, 500), wxSize(260, 45));
-  m_txt1 = new wxTextCtrl(this, wxID_ANY, "placeholder", wxPoint(10, 70), wxSize(150, 50));
-  m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(150, 50));
+  m_txt1 = new wxTextCtrl(this, wxID_ANY, "placeholder", wxPoint(20, 20), wxSize(260, 50));
+  m_list1 = new wxListBox(this, wxID_ANY, wxPoint(20, 70), wxSize(260, 250));
+  m_btn1 = new wxButton(this, wxID_ANY, "bora bora", wxPoint(40, 370), wxSize(220, 45));
 }
 cmain::~cmain() {}
